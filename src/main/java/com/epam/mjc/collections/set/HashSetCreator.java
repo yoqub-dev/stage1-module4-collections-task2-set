a78ad7a9d3a36ff7e95261e6a1d33ea3112032b9
yoqub-dev <yoqub.epam@gmail.com>
2023-11-14 14:26:03 +0500

    task7

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -9,6 +9,7 @@ public class HashSetCreator {
 
         for (int x : sourceList) {
             if (x % 2 == 0) {
+                result.add(1); // Adding 1 for even numbers
                 while (x % 2 == 0) {
                     result.add(x);
                     x /= 2;
