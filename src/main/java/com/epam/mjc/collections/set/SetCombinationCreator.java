8fbbc3e2747a671506d2acb245f97f6a67a21522
yoqub-dev <yoqub.epam@gmail.com>
2023-11-14 15:53:39 +0500

    task7

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -1,25 +1,20 @@
 package com.epam.mjc.collections.set;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
-        Set<String> result = new HashSet<>();
+        Set<String> result = new HashSet<>(firstSet);
+        result.retainAll(secondSet);
+        result.removeAll(thirdSet);
 
-        // Elements that belong to the first and the second Sets and not belong to the third Set
-        for (String element : firstSet) {
-            if (secondSet.contains(element) && !thirdSet.contains(element)) {
-                result.add(element);
-            }
-        }
+        Set<String> exclusiveThirdSetElements = new HashSet<>(thirdSet);
+        exclusiveThirdSetElements.removeAll(firstSet);
+        exclusiveThirdSetElements.removeAll(secondSet);
 
-        // Elements that belong only to the third Set
-        result.addAll(thirdSet);
+        result.addAll(exclusiveThirdSetElements);
 
-        // Sort the result set to make the order consistent based on names
-        List<String> resultList = new ArrayList<>(result);
-        Collections.sort(resultList);
-
-        return new LinkedHashSet<>(resultList);
+        return result;
     }
 }
